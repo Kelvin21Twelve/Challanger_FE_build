dfb44ceec0e39052132b6e15f29983271d119e0b
kelvin21 <kelvin@21twelveinteractive.in>
2026-01-05 19:13:19 +0530

    Receipt update

--- src/queries/useRefundReceipt.js
+++ src/queries/useRefundReceipt.js
@@ -8,7 +8,7 @@ export function useRefundReceipt() {
     mutationKey: ["get-refund-receipt"],
     mutationFn: async (job_id) => {
       const response = await axios.post(getRefundReceipt, { job_id });
-      const data = response?.data?.data;
+      const data = response?.data;
       const { success, view } = data || {};
 
       if (success && view && typeof window !== "undefined") window.open(view);
